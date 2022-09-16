1d84ce8f459f6be862e585bb6feea5d9e58b0ce2
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-09-16 13:59:35 +0300

    fix js 6-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,9 @@
+// Напиши скрипт для создания галереи изображений по массиву данных. 
+// В HTML есть список ul.gallery.
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -17,20 +23,20 @@ const imagesGallery = document.querySelector('.gallery');
 console.log(imagesGallery);
 
 const catGallery = images.map(function ({url, alt}) {
-   const liHtml = `<li class = imgLi>
+   return `<li class = imgLi>
    <img class = img src=${url} alt=${alt} width = 1260>
     </li>` 
-    imagesGallery.insertAdjacentHTML("beforeend", liHtml);
-       
-  })
+})
+   imagesGallery.insertAdjacentHTML("beforeend", catGallery);
+       console.log('catGallery', catGallery)
  
 imagesGallery.style.listStyle = "none";
 imagesGallery.style.margin = '0';
 imagesGallery.style.padding = '0';
 imagesGallery.style.display = "grid"; 
 imagesGallery.style.gridTemplateColumns = 'repeat(1)';
-imagesGallery.style.gridAutoRows = 'minmax(300px, auto)';
-imagesGallery.style.gridRowGap = '3em';
+imagesGallery.style.gridAutoRows = 'minmax(30px, auto)';
+// imagesGallery.style.gridRowGap = '1em';
  
 // const catGallery = 
 //   images.forEach(function (image, index) {
